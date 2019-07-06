abfaea530ec877f9bcd7a7482651d18278a1b81c
Pei Lin Li <peilin.li@yahoo.ca>
2019-07-06 15:57:55 -0400

    skeleton for enventual code.

---
+++ src/parameters.py
@@ -0,0 +1,9 @@
+# data paths
+TRAIN_DATA_DIR = ""
+TEST_DATA_DIR = ""
+
+# Shuffle buffer size (>= dataset size for perfect shuffling)
+BUFFER_SIZE = 400
+
+# This should actually be much more but also that will blow up the RAM
+EPOCHS = 4
