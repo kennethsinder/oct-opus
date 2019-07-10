f3f144c4a3bf9a7b7c9ad4dc5964f242cde8a79a
Pei Lin Li <peilin.li@yahoo.ca>
2019-07-10 18:34:48 -0400

    refactored out major components.

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,34 @@ import re
 import tensorflow as tf
 
 
+def resize(input_image, real_image, height, width):
+    input_image = tf.image.resize(input_image, [height, width], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    real_image = tf.image.resize(real_image, [height, width], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    return input_image, real_image
+
+
+def random_crop(input_image, real_image):
+    stacked_image = tf.stack([input_image, real_image], axis=0)
+    cropped_image = tf.image.random_crop(stacked_image, size=[2, 256, 256, 1])
+    return cropped_image[0], cropped_image[1]
+
+
+@tf.function()
+def random_jitter(input_image, real_image):
+    # resizing to 286 x 286
+    input_image, real_image = resize(input_image, real_image, 286, 286)
+
+    # randomly cropping to 256 x 256
+    input_image, real_image = random_crop(input_image, real_image)
+
+    if tf.random.uniform(()) > 0.5:
+        # random mirroring
+        input_image = tf.image.flip_left_right(input_image)
+        real_image = tf.image.flip_left_right(real_image)
+
+    return input_image, real_image
+
+
 # Decodes a grayscale PNG, returns a 2D tensor.
 def load_image(file_name):
     image = tf.io.read_file(file_name)
@@ -18,15 +46,22 @@ def load_image(file_name):
 # (OMAG Bscans/1.png corresponds to xzIntensity/{1,2,3,4}.png.)
 def get_images(bscan_path):
     bscan_img = load_image(bscan_path)
+
     path_components = re.search(r'^(.*)xzIntensity/(\d+)\.png$', bscan_path)
 
     dir_path = path_components.group(1)
     bscan_num = int(path_components.group(2))
 
     omag_num = ((bscan_num - 1) // 4) + 1
+
     omag_img = load_image(os.path.join(dir_path, 'OMAG Bscans', '{}.png'.format(omag_num)))
 
     bscan_img = tf.cast(bscan_img, tf.float32)
     omag_img = tf.cast(omag_img, tf.float32)
+
+    bscan_img = (bscan_img / 127.5) - 1
+    omag_img = (omag_img / 127.5) - 1
+
+    bscan_img, omag_img = random_jitter(bscan_img, omag_img)
 
     return bscan_img, omag_img
