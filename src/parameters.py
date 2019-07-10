68cadf44b8230106e681cb6c6b2110782e86fd00
Pei Lin Li <peilin.li@yahoo.ca>
2019-07-10 18:56:44 -0400

    moved parameters out of run file.

--- src/parameters.py
+++ src/parameters.py
@@ -1,9 +1,14 @@
-# data paths
-TRAIN_DATA_DIR = ""
-TEST_DATA_DIR = ""
+# Set these to be the directories containing the data subdirectories
+# (so xzIntensity and OMAG BScans are 2 levels down from this dir).
+TRAIN_DATA_DIR = './train'
+TEST_DATA_DIR = './test'
 
 # Shuffle buffer size (>= dataset size for perfect shuffling)
 BUFFER_SIZE = 400
 
 # This should actually be much more but also that will blow up the RAM
-EPOCHS = 4
+EPOCHS = 100
+
+# Used by generator
+OUTPUT_CHANNELS = 1
+
