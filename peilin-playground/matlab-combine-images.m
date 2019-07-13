f98bff829de5625662c4b25f8563c667d0fdd3e6
Pei Lin Li <peilin.li@yahoo.ca>
2019-07-13 01:02:49 -0400

    making matlab script more use friendly.

--- peilin-playground/matlab-combine-images.m
+++ peilin-playground/matlab-combine-images.m
@@ -1,30 +1,37 @@
-% clean environment
+%% clean environment
 clear
 clc
 
 %% source and destination
-src = '/private/fydp1/oct-opus-data/2015-09-07-Images-46/';
-dst = '/private/fydp1/enface-images/2015-09-07-Images-46/';
+datestamp = input('please provide datestamp for /private/fydp1/oct-opus-data/??? : ', 's');
+src = strcat('/private/fydp1/oct-opus-data/', datestamp, '/');
+if not(exist(src, 'dir'))
+    disp(['directory', src, ' does not exist ... exiting'])
+    exit
+end
+dst = strcat('/private/fydp1/enface-images/', datestamp, '/');
 mkdir([dst]);
 
-% number of images
+%% number of images
 N = length(dir([src, '*.png']));
+disp([num2str(N), ' images found']);
 
-%% load images into matlab
+%% load z-axis cross-sections into matlab to build a "cube"
 for i = 1:N
-    disp(i)
-    iMat = im2double(imread([src, int2str(i), '.png']));
-    Int(:,:,i) = iMat;
+    SingleCrossSection = im2double(imread([src, int2str(i), '.png']));
+    Scans(:,:,i) = SingleCrossSection;
 end
+disp('Step 1/3 Complete');
 
-%% number of vertical pixels (depth)
+%% re-slice the "cube" along the y-axis with squeeze and resize
 for i = 1:256
-    disp(i)
-    dEnFace = imresize(squeeze(Int(i,:,:)), [1000 1000]);
-    imwrite(dEnFace, [dst, int2str(i), '.png']);
+    EnFace = imresize(squeeze(Scans(i,:,:)), [1000 1000]);
+    imwrite(EnFace, [dst, int2str(i), '.png']);
 end
+disp('Step 2/3 Complete');
 
-% the depth you want to have the Max_Proj image over it
+% the depth you want to have the max proj image over it
+% from experiments, it was found that for our images, layers 30-55 provide good results
 count = 1;
 for i = 30:55
     Im = im2double(imread([dst, int2str(i), '.png']));
@@ -32,6 +39,7 @@ for i = 30:55
     Layers(:,:,count) = Im;
     count = count + 1;
 end
+disp('Step 3/3 Complete');
 
 S = imresize(sum(Layers, 3), [1000 1000]);
 m = imresize(max(Layers, [], 3), [1000 1000]);
