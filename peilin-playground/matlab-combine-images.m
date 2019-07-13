9005d1fc38c4e7de787a70394ae1103ebd3c9af7
Pei Lin Li <peilin.li@yahoo.ca>
2019-07-13 00:26:05 -0400

    some what functional matlab code.

--- peilin-playground/matlab-combine-images.m
+++ peilin-playground/matlab-combine-images.m
@@ -8,7 +8,7 @@ dst = '/private/fydp1/enface-images/2015-09-07-Images-46/';
 mkdir([dst]);
 
 % number of images
-N = length(dir([src, '*.png'])) - length(dir([src, '*_*.png']));
+N = length(dir([src, '*.png']));
 
 %% load images into matlab
 for i = 1:N
@@ -18,23 +18,22 @@ for i = 1:N
 end
 
 %% number of vertical pixels (depth)
-for kk = 1:1000
-    disp(kk)
-    dEnFace = imresize(squeeze(Int(kk,:,:)), [1000 1000]);
-    imwrite(dEnFace, [dst, int2str(kk), '.png']);
+for i = 1:256
+    disp(i)
+    dEnFace = imresize(squeeze(Int(i,:,:)), [1000 1000]);
+    imwrite(dEnFace, [dst, int2str(i), '.png']);
 end
 
-count = 1;
-
 % the depth you want to have the Max_Proj image over it
-for i = 680:750
+count = 1;
+for i = 30:55
     Im = im2double(imread([dst, int2str(i), '.png']));
     Im = Im./max(max(Im)); % this is for normalizing the image
-    Int(:,:,count) = Im;
+    Layers(:,:,count) = Im;
     count = count + 1;
 end
 
-S = imresize(sum(Int,3),[1000 1000]);
-m = imresize(max(Int,[],3),[1000 1000]);
-figure,imshow(m./max(max(m)),[])
-figure,imshow(S,[])
+S = imresize(sum(Layers, 3), [1000 1000]);
+m = imresize(max(Layers, [], 3), [1000 1000]);
+figure,imshow(m./max(max(m)), [])
+figure,imshow(S, [])
