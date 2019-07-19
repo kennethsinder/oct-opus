700fa9df7b6ac7d3b66f68ae51ddadfee68cc474
Pei Lin Li <peilin.li@yahoo.ca>
2019-07-19 18:10:27 -0400

    new naming convention for data sources.

--- src/parameters.py
+++ src/parameters.py
@@ -1,7 +1,7 @@
 # Set these to be the directories containing the data subdirectories
 # (so xzIntensity and OMAG BScans are 2 levels down from this dir).
-TRAIN_DATA_DIR = '/private/fydp1/training-data/2015-08-11-Images-50'
-TEST_DATA_DIR = '/private/fydp1/testing-data/2015-09-07-Images-46'
+TRAIN_DATA_DIR = '/private/fydp1/training-data/cleaned-2015-08-11-Images-50'
+TEST_DATA_DIR = '/private/fydp1/testing-data/cleaned-2015-09-07-Images-46'
 
 # Shuffle buffer size (>= dataset size for perfect shuffling)
 BUFFER_SIZE = 400
