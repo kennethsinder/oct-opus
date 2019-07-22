1adf9505e9db7051455fce7b5dbd402afed04644
Pei Lin Li <peilin.li@yahoo.ca>
2019-07-22 15:52:40 -0400

    allows sourcing from multiple data sets.

--- src/parameters.py
+++ src/parameters.py
@@ -1,7 +1,6 @@
-# Set these to be the directories containing the data subdirectories
-# (so xzIntensity and OMAG BScans are 2 levels down from this dir).
-TRAIN_DATA_DIR = '/private/fydp1/training-data/cleaned-2015-08-11-Images-50'
-TEST_DATA_DIR = '/private/fydp1/testing-data/cleaned-2015-09-07-Images-46'
+# Training and testing data set locations
+TRAIN_DATA_DIR = '/private/fydp1/training-data'
+TEST_DATA_DIR = '/private/fydp1/testing-data'
 
 # Shuffle buffer size (>= dataset size for perfect shuffling)
 BUFFER_SIZE = 400
@@ -11,4 +10,3 @@ EPOCHS = 100
 
 # Used by generator
 OUTPUT_CHANNELS = 1
-
