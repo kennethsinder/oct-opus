ffaf24b32c30806287c246f27f35ad4011eb791a
Kenneth Sinder <kennethsinder@gmail.com>
2019-07-28 12:57:34 -0400

    Fix a few issues affecting results

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-NUM_EPOCHS=100
+NUM_EPOCHS=20
 
-python run.py train 0
+python run.py train 7
 
-STARTING_EPOCH=1
+STARTING_EPOCH=8
 for i in $( seq $STARTING_EPOCH $NUM_EPOCHS )
 do
     python run.py restore train $i
