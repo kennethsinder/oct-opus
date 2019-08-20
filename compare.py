8b9f89d1c15f3c3084a604fa01889f49896cbbda
Pei Lin Li <peilin.li@yahoo.ca>
2019-08-20 14:02:00 -0400

    added wiki links and pae and mae implementations.

--- compare.py
+++ compare.py
@@ -3,35 +3,47 @@ import numpy as np
 from PIL import Image
 
 
-# TODO: peak absolute error
+def fsim(image_a, image_b):
+    pass
+
+
 def pae(image_a, image_b):
-    pass
+    # note: this isn't actually particularly well defined or commonly used metric, but including it just in case
+    return np.max(np.absolute((image_a.astype("float") - image_b.astype("float"))))
 
 
-# TODO: mean absolute error
 def mae(image_a, image_b):
-    pass
+    # see https://en.wikipedia.org/wiki/Mean_absolute_error
+    error_sum = np.sum(np.absolute((image_a.astype("float") - image_b.astype("float"))))
+    return error_sum / (image_a.shape[0] * image_a.shape[1])
 
 
 def compare_all(image_a, image_b):
-    # peak signal to noise ratio
+    # peak signal to noise ratio (see https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio)
     psnr_score = compare_psnr(image_a, image_b)
-    print("PSNR: {}".format(str(psnr_score)), end="\n\n")
+    print("PSNR: {}".format(str(psnr_score)), end=" dB\n\n")
 
-    # mean squared error
+    # mean squared error (see https://en.wikipedia.org/wiki/Mean_squared_error)
     mse_score = compare_mse(image_a, image_b)
     print("MSE: {}".format(str(mse_score)))
     print("Range [0, +INF) where 0 is identical", end="\n\n")
 
-    # normalized root mean squared error
+    # normalized root mean squared error (see https://en.wikipedia.org/wiki/Root-mean-square_deviation)
     nrmse_score = compare_nrmse(image_a, image_b, norm_type='Euclidean')
     print("NRMSE: {}".format(str(nrmse_score)), end="\n\n")
 
-
-    # structural similarity measure
+    # structural similarity measure (see https://en.wikipedia.org/wiki/Structural_similarity)
     ssim_score = compare_ssim(image_a, image_b, full=False)
     print("SSIM: {}".format(str(ssim_score)))
-    print("Range [-1, +1] where +1 is identical")
+    print("Range [-1, +1] where +1 is identical", end="\n\n")
+
+    pae_score = pae(image_a, image_b)
+    print("PAE: {}".format(str(pae_score)))
+    print("Range [0, +INF) where 0 is identical", end="\n\n")
+
+    mae_score = mae(image_a, image_b)
+    print("MAE: {}".format(str(mae_score)))
+    print("Range [0, +INF) where 0 is identical", end="\n\n")
 
 
 if __name__ == '__main__':
@@ -42,7 +54,7 @@ if __name__ == '__main__':
     image_b_obj = Image.open(image_b_path)
 
     assert image_a_obj.size == image_b_obj.size
-    print("Both images are " + str(image_a_obj.size))
+    print("Both images are " + str(image_a_obj.size), end="\n\n")
 
     image_a = np.asarray(image_a_obj)
     image_b = np.asarray(image_b_obj)
