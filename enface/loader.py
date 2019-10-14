0aa68ade9378c7fae85316b822a212049a356dce
Pei Lin Li <peilin.li@yahoo.ca>
2019-10-14 16:31:48 -0400

    added image enhancement capabilities.

--- enface/loader.py
+++ enface/loader.py
@@ -1,6 +1,6 @@
 import PIL.ImageOps
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageEnhance
 from enum import Enum
 from os import listdir
 from os.path import join
@@ -16,13 +16,19 @@ class Loader:
         self.input_type = input_type
         self.image_dimensions = image_dimensions
 
-    def invert_color_scheme(self, filename):
-        return PIL.ImageOps.invert(self.load_single_image(filename))
+    def invert_color_scheme(self, filename, enable_enhancement):
+        return PIL.ImageOps.invert(self.load_single_image(filename, enable_enhancement))
 
-    def load_single_image(self, filename):
-        return Image.open(filename)
+    def load_single_image(self, filename, enable_enhancement):
+        original_image = Image.open(filename)
+        if not enable_enhancement:
+            return original_image
+        contrast_enhancer = ImageEnhance.Contrast(original_image)
+        contrast_image = contrast_enhancer.enhance(1.5)
+        sharpness_enhancer = ImageEnhance.Sharpness(contrast_image)
+        return sharpness_enhancer.enhance(2.0)
 
-    def load_data_set(self):
+    def load_data_set(self, enable_enhancement=False):
         num_images = len(listdir(self.src_dir))
         if num_images == 0:
             raise ValueError('FoundZeroImages')
@@ -34,9 +40,9 @@ class Loader:
             try:
                 j += 1
                 if self.input_type == self.InputType.BSCAN:
-                    img = self.invert_color_scheme(join(self.src_dir, '{}.png'.format(j)))
+                    img = self.invert_color_scheme(join(self.src_dir, '{}.png'.format(j)), enable_enhancement)
                 else:
-                    img = self.load_single_image(join(self.src_dir, '{}.png'.format(j)))
+                    img = self.load_single_image(join(self.src_dir, '{}.png'.format(j)), enable_enhancement)
                 eye[:, :, i] = np.asarray(img)
             except FileNotFoundError:
                 i -= 1
