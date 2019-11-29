e119de117e74886635927d75babeecca779e09a6
Pei Lin Li <pl3li@edu.uwaterloo.ca>
2019-11-29 01:02:00 -0500

    better cmdline scripts. (#75)

--- enface/enface.py
+++ enface/enface.py
@@ -1,13 +1,9 @@
-import sys
-from glob import glob
-from os import makedirs, chdir
-from os.path import isfile, join, basename, normpath
-
-from enface.image_io import ImageIO
-from enface.slicer import Slicer
+from os.path import join
 
 from configs.parameters import START_ROW, END_ROW
 from datasets.train_and_test import TESTING_DATASETS
+from enface.image_io import ImageIO
+from enface.slicer import Slicer
 
 
 def gen_single_enface(input_dir, input_type: ImageIO.InputType, output_dir):
@@ -29,46 +25,3 @@ def gen_enface_all_testing(predicted_dir):
             input_type=ImageIO.InputType.OMAG,
             output_dir=join(predicted_dir, test_dataset)
         )
-
-
-def bulk_enface():
-    for folder in glob(join(sys.argv[1], '*')):
-        if isfile(folder):
-            continue
-        folder = basename(normpath(folder))
-        full_folder_path = join(sys.argv[1], folder)
-        for subfolder in glob(join(full_folder_path, '*')):
-            if isfile(subfolder):
-                continue
-            subfolder = basename(normpath(subfolder))
-            # At this point `subfolder` is either "OMAG Bscans",
-            # "xzOMAGInt", or "OMAG Bscans" i.e. the subfolders within
-            # each eye data set.
-            full_subfolder_path = join(full_folder_path, subfolder)
-            makedirs(subfolder, exist_ok=True)
-            chdir(subfolder)
-            try:
-                # Call the enface script
-                # main([None, full_subfolder_path, folder])
-                # TODO: call enface function
-                pass
-            except IndexError:
-                # Error from enface script: No files in the directory
-                continue
-            chdir('..')
-
-
-if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        print("python enface.py input_dir img_type={omag,bscan} output_dir")
-        raise Exception("MissingArgument")
-
-    if str(sys.argv[2]).lower() == "omag":
-        img_type = ImageIO.InputType.OMAG
-    elif str(sys.argv[2]).lower() == "bscan":
-        img_type = ImageIO.InputType.BSCAN
-    else:
-        print("python enface.py input_dir img_type={omag,bscan} output_dir")
-        raise Exception("InvalidImageType")
-
-    gen_single_enface(input_dir=sys.argv[1], input_type=img_type, output_dir=sys.argv[3])
