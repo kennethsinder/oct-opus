2870c9a55c10a3ed46c3fd718ecfe5b42e269bcb
Pei Lin Li <pl3li@edu.uwaterloo.ca>
2020-04-25 13:49:26 -0400

    moved run.py to cgan directory.

--- run.sh
+++ run.sh
@@ -27,5 +27,5 @@ fi
 LOGDIR='logs/'$(date +"%d-%m-%Y_%H:%M:%S")
 
 echo "Logs are being sent to $LOGDIR..."
-python run.py --logdir $LOGDIR --starting-epoch $STARTING_EPOCH --ending-epoch $ENDING_EPOCH --datadir $DATA_DIR train $HARDWARE
+python cgan/run.py --logdir $LOGDIR --starting-epoch $STARTING_EPOCH --ending-epoch $ENDING_EPOCH --datadir $DATA_DIR train $HARDWARE
 
