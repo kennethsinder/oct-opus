99e67f22dce03870db0730279b37e816b7d89e33
Pei Lin Li <pl3li@edu.uwaterloo.ca>
2020-05-07 17:04:41 -0400

    more changes to datasets class.

--- cgan/dataset.py
+++ cgan/dataset.py
@@ -1,22 +1,27 @@
+import glob
 import random
 from os import listdir
 from os.path import join, isdir
+from typing import List, Tuple, Set
+
+from cgan.parameters import OMAG_DIRNAME, BSCAN_DIRNAME
 
 
 class Dataset:
 
     def __init__(self, root_data_path, num_folds=1, seed=42):
-        self.__root_data_path = root_data_path
-        ls = set(listdir(root_data_path))
+        self.__root_data_path: str = root_data_path
+        temp = set(listdir(root_data_path))
+        ls = map(lambda x: join(root_data_path, x), temp)
         # save only the directories
-        self.__all_datasets = set(filter(lambda x: isdir(join(root_data_path, x)), ls))
+        self.__all_datasets: Set = set(filter(lambda x: isdir(join(root_data_path, x)), ls))
         assert len(self.__all_datasets) > 0
 
         # sets the seed for random
         random.seed(seed)
 
         # partitions the dataset into folds
-        self.__folds = []
+        self.__folds: List[Set] = []
         usable_datasets_copy = self.__all_datasets.copy()
         for i in range(num_folds - 1):
             self.__folds.append(set(
@@ -28,13 +33,27 @@ class Dataset:
             usable_datasets_copy = usable_datasets_copy - self.__folds[i]
         self.__folds.append(usable_datasets_copy)
 
-    def get_train_and_test_by_fold_id(self, fold_id):
+    def get_train_and_test_by_fold_id(self, fold_id) -> Tuple[Set, Set]:
         test_sets = self.__folds[fold_id]
         train_sets = self.__all_datasets - test_sets
         return train_sets, test_sets
 
-    def get_all_datasets(self):
+    def get_all_datasets(self) -> Set:
         return self.__all_datasets.copy()
 
-    def get_all_folds(self):
+    def get_all_folds(self) -> List[Set]:
         return self.__folds.copy()
+
+    def get_all_bscans(self) -> List:
+        __image_files = []
+        for dataset_path in self.__all_datasets:
+            __image_files.extend(glob.glob(join(dataset_path, BSCAN_DIRNAME, '[0-9]*.png')))
+        assert len(__image_files) > 0
+        return __image_files
+
+    def get_all_omags(self) -> List:
+        __image_files = []
+        for dataset_path in self.__all_datasets:
+            __image_files.extend(glob.glob(join(dataset_path, OMAG_DIRNAME, '[0-9]*.png')))
+        assert len(__image_files) > 0
+        return __image_files
