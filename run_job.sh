c707522e58d947eed1ebfb98136319f70b1d1409
Kenneth Sinder <kennethsinder@gmail.com>
2020-05-08 13:55:25 -0400

    Go back to using full dataset now that verification is ok

--- run_job.sh
+++ run_job.sh
@@ -9,9 +9,9 @@
 cd $SLURM_TMPDIR
 # No longer using original (non-flattened) data for now:
 # tar --use-compress-program=pigz -xf ~/projects/def-vengu/s2saberi/all_data_original.tar.gz
-unzip ~/projects/def-vengu/s2saberi/all_data_flattened_2.zip
+unzip ~/projects/def-vengu/s2saberi/all_data_flattened.zip
 
 cd -
 module load cuda/10.0
-./run.sh "$@" $SLURM_TMPDIR
+./run.sh "$@" $SLURM_TMPDIR/all_data_flattened
 
