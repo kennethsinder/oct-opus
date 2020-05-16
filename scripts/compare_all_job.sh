36996cb7b126c0d26ea0bf5c152584853aa3de46
Kenneth Sinder <kennethsinder@gmail.com>
2020-05-16 16:13:56 -0700

    Update comment

--- scripts/compare_all_job.sh
+++ scripts/compare_all_job.sh
@@ -4,10 +4,12 @@
 #SBATCH --gres=gpu:1
 
 # NOTE: Change `def-vengu` to the appropriate Sharcnet account.
-# Usage: `sbatch compare_all_job.sh <experiment_dir>`
+#
+# Usage: `sbatch ./scripts/compare_all_job.sh <experiment_dir>`
 # The `experiment_dir` is passed faithfully along to `compare_all.py`.
-# The flattened sedated rat data is assumed to be one level up from
-# the execution of this script, and is extracted for you and that extract
+# The flattened sedated rat data `all_data_flattened.zip` 
+# is assumed to be one level up from the execution of this script.
+# It is extracted for you and that extract
 # location is fed into the `compare_all.py` script so you don't have to do
 # that work on Sharcnet/Graham. `compare_all.py` is assumed to be in
 # the `./scripts` folder.
