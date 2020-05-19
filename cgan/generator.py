41364c30c6938bc7d148e11d6298c786e8cd16e2
Kenneth Sinder <kennethsinder@gmail.com>
2020-05-19 12:58:19 -0700

    Add down and upsample blocks that never got committed

--- cgan/generator.py
+++ cgan/generator.py
@@ -7,9 +7,10 @@ from cgan.sampling import downsample, upsample
 
 def generator():
     down_stack = [
-        downsample(64, 4, apply_batchnorm=False),  # (bs, 128, 128, 64)
-        downsample(128, 4),  # (bs, 64, 64, 128)
-        downsample(256, 4),  # (bs, 32, 32, 256)
+        downsample(64, 4, apply_batchnorm=False),  # (bs, 256, 256, 64)
+        downsample(128, 4),  # (bs, 128, 128, 128)
+        downsample(256, 4),  # (bs, 64, 64, 256)
+        downsample(512, 4),  # (bs, 32, 32, 512)
         downsample(512, 4),  # (bs, 16, 16, 512)
         downsample(512, 4),  # (bs, 8, 8, 512)
         downsample(512, 4),  # (bs, 4, 4, 512)
@@ -22,9 +23,10 @@ def generator():
         upsample(512, 4, apply_dropout=True),  # (bs, 4, 4, 1024)
         upsample(512, 4, apply_dropout=True),  # (bs, 8, 8, 1024)
         upsample(512, 4),  # (bs, 16, 16, 1024)
-        upsample(256, 4),  # (bs, 32, 32, 512)
-        upsample(128, 4),  # (bs, 64, 64, 256)
-        upsample(64, 4),  # (bs, 128, 128, 128)
+        upsample(512, 4),  # (bs, 32, 32, 1024)
+        upsample(256, 4),  # (bs, 64, 64, 512)
+        upsample(128, 4),  # (bs, 128, 128, 256)
+        upsample(64, 4),  # (bs, 256, 256, 128)
     ]
 
     initializer = tf.random_normal_initializer(0., 0.02)
@@ -32,7 +34,7 @@ def generator():
                                            strides=2,
                                            padding='same',
                                            kernel_initializer=initializer,
-                                           activation='tanh')  # (bs, 256, 256, 3)
+                                           activation='tanh')  # (bs, 512, 512, 3)
 
     concat = tf.keras.layers.Concatenate()
 
