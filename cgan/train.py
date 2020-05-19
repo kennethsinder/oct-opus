9abb4f595383d80bebf3d253319287f4e6728d40
Pei Lin Li <pl3li@edu.uwaterloo.ca>
2020-05-19 17:37:40 -0400

    Add remaining gen losses from pix2pix tutorial (#103)
    Co-authored-by: Kenneth Sinder <kennethsinder@gmail.com>

--- cgan/train.py
+++ cgan/train.py
@@ -12,30 +12,34 @@ def train_step(model_state, input_image, target):
         disc_real_output = model_state.discriminator([input_image, target], training=True)
         disc_generated_output = model_state.discriminator([input_image, gen_output], training=True)
 
-        gen_loss = generator_loss(model_state.loss_object, disc_generated_output, gen_output, target)
+        gen_total_loss, gen_gan_loss, gen_l1_loss = generator_loss(model_state.loss_object, disc_generated_output,
+                                                                   gen_output, target)
         disc_loss = discriminator_loss(model_state.loss_object, disc_real_output, disc_generated_output)
 
-    generator_gradients = gen_tape.gradient(gen_loss, model_state.generator.trainable_variables)
+    generator_gradients = gen_tape.gradient(gen_total_loss, model_state.generator.trainable_variables)
     discriminator_gradients = disc_tape.gradient(disc_loss, model_state.discriminator.trainable_variables)
 
     model_state.generator_optimizer.apply_gradients(zip(generator_gradients, model_state.generator.trainable_variables))
-    model_state.discriminator_optimizer.apply_gradients(zip(discriminator_gradients, model_state.discriminator.trainable_variables))
+    model_state.discriminator_optimizer.apply_gradients(zip(discriminator_gradients,
+                                                            model_state.discriminator.trainable_variables))
 
-    return gen_loss, disc_loss
+    return gen_total_loss, gen_gan_loss, gen_l1_loss, disc_loss
 
 
 def train_epoch(tbd_writer, train_dataset, model_state, epoch_num):
-    gen_loss_sum = 0
-    disc_loss_sum = 0
+    gen_total_loss_sum, gen_gan_loss_sum, gen_l1_loss_sum, disc_loss_sum = 0, 0, 0, 0
     index = 0
     for input_image, target in train_dataset:
-        gen_loss, disc_loss = train_step(model_state, input_image, target)
-        gen_loss_sum += gen_loss
+        gen_total_loss, gen_gan_loss, gen_l1_loss, disc_loss = train_step(model_state, input_image, target)
+        gen_total_loss_sum += gen_total_loss
+        gen_gan_loss_sum += gen_gan_loss
+        gen_l1_loss_sum += gen_l1_loss
         disc_loss_sum += disc_loss
-        model_state.current_training_step += 1
         index += 1
 
         # log info to tensorboard
         with tbd_writer.as_default():
-            tf.summary.scalar('avg_gen_loss', gen_loss_sum / index, step=epoch_num)
+            tf.summary.scalar('avg_gen_total_loss', gen_total_loss_sum / index, step=epoch_num)
+            tf.summary.scalar('avg_gen_gan_loss', gen_gan_loss_sum / index, step=epoch_num)
+            tf.summary.scalar('avg_l1_loss', gen_l1_loss_sum / index, step=epoch_num)
             tf.summary.scalar('avg_disc_loss', disc_loss_sum / index, step=epoch_num)
