b482bd014264cdf88d5366ee4700857eba61eb88
Kenneth Sinder <kennethsinder@gmail.com>
2020-05-27 09:55:38 -0700

    reflattened

--- run_job.sh
+++ run_job.sh
@@ -12,10 +12,8 @@
 # to the `cgan.py` script.
 
 cd $SLURM_TMPDIR
-# No longer using original (non-flattened) data for now:
-# tar --use-compress-program=pigz -xf ~/projects/def-vengu/s2saberi/all_data_original.tar.gz
-unzip -qq ~/projects/def-vengu/s2saberi/all_data_flattened.zip
+tar --use-compress-program=pigz -xf ~/projects/def-vengu/s2saberi/all_data_reflattened.tar.gz
 
 cd -
 module load cuda/10.0
-./run.sh --num-epochs "$1" --datadir "$SLURM_TMPDIR"/all_data_flattened "${@:2}" train
+./run.sh --num-epochs "$1" --datadir "$SLURM_TMPDIR"/all_data_reflattened "${@:2}" train
