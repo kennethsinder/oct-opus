30f40c6f81dda03775e4e8f3ff9c6fc0daae611d
Kathleen Wang <k262wang@uwaterloo.ca>
2020-07-19 16:18:55 -0400

    Add single_poly_flattened stats

--- cnn/parameters.py
+++ cnn/parameters.py
@@ -5,3 +5,14 @@ NUM_IMAGES_PER_DATASET = 512
 
 IMAGE_DIM = 512
 PIXEL_DEPTH = 256
+
+STATS = {
+    'single_poly_flattened' : {
+        0.8 : {
+            42 : {
+                'mean' : 0.9211185511374553,
+                'std' : 0.16026184161479942
+            }
+        }
+    }
+}
